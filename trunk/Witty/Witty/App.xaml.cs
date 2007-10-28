5e99dd32d28413027aab3e48c8d0ab488d513e92
alan.le <alan.le@95ee034e-633c-0410-b380-e75d38d4a260>
2007-10-28 08:59:50 +0000

    Added skinning support Created Aero skin made username focused by default on the login screen added skin styles test files

--- trunk/Witty/Witty/App.xaml.cs
+++ trunk/Witty/Witty/App.xaml.cs
@@ -4,6 +4,9 @@ using System.Data;
 using System.Xml;
 using System.Configuration;
 using TwitterLib;
+using System.Collections.Specialized;
+using System.IO;
+using System.Globalization;
 
 namespace Witty
 {
@@ -15,5 +18,52 @@ namespace Witty
     {
         public static User LoggedInUser = null;
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Properties.Settings appSettings = Witty.Properties.Settings.Default;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(appSettings.Skin))
+                {
+                    ResourceDictionary rd = new ResourceDictionary();
+                    rd.MergedDictionaries.Add(Application.LoadComponent(new Uri(appSettings.Skin, UriKind.Relative)) as ResourceDictionary);
+                    Application.Current.Resources = rd;
+                }
+            }
+            catch
+            {
+            }
+
+
+            base.OnStartup(e);
+        }
+
+        /// <summary>
+        /// Gets the collection of skins
+        /// </summary>
+        public static NameValueCollection Skins
+        {
+            get
+            {
+                NameValueCollection skins = new NameValueCollection();
+
+                foreach (string folder in Directory.GetDirectories(@".\"))
+                {
+                    foreach (string file in Directory.GetFiles(folder))
+                    {
+                        FileInfo fileInfo = new FileInfo(file);
+                        if (string.Compare(fileInfo.Extension, ".xaml",
+                            true, CultureInfo.InvariantCulture) == 0)
+                        {
+                            // Use the first part of the resource file name for the menu item name.
+                            skins.Add(fileInfo.Name.Remove(fileInfo.Name.IndexOf(".xaml")),
+                                Path.Combine(folder, fileInfo.Name));
+                        }
+                    }
+                }
+                return skins;
+            }
+        }
     }
 }
