ee3bea40a8e2012914b3d373db14666c5e4adb20
alan.le <alan.le@95ee034e-633c-0410-b380-e75d38d4a260>
2008-03-24 23:03:54 +0000

    performance and scrolling changes smooth scrolling is enabled again truncating the list of tweets to 100 for perf. created unit tests

---
+++ trunk/Witty/WittyUnitTests/TweetTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using TwitterLib;
+
+namespace WittyUnitTests
+{
+    [TestFixture]
+    public class TweetTest
+    {
+        protected int count;
+
+        [SetUp]
+        public void Init()
+        {
+            count = 10;
+        }
+
+        [Test]
+        public void Truncate()
+        {
+            TweetCollection tweets = new TweetCollection();
+
+            for (int i = 0; i < 20; i++)
+            {
+                Tweet tweet = new Tweet();
+                tweet.Id = i;
+                tweets.Insert(0, tweet);
+            }
+
+            tweets.Truncate(count);
+
+            Assert.AreEqual(count, tweets.Count);
+            Assert.AreEqual(tweets[0].Id, 19);
+        }
+    }
+}
