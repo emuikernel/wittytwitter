a94780fe534ae2d7fe4ba42c4752b99617af5003
keith.elder <keith.elder@95ee034e-633c-0410-b380-e75d38d4a260>
2008-03-29 15:49:52 +0000

    This checkin enables ClickOnce deployment for Witty.  There is a new options screen called "Updates".  This is only enabled when the application is ClickOnce deployed.  There are new setting properties used to control how often Witty will check for automatic updates while it is running.

---
+++ Witty/Witty/ClickOnce/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Deployment.Application;
+
+namespace Witty.ClickOnce
+{
+    public class Utils
+    {
+        public static bool IsApplicationNetworkDeployed
+        {
+            get
+            {
+                if (ApplicationDeployment.IsNetworkDeployed)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
