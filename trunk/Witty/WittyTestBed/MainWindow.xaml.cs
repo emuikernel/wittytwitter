1179f3a205393207fb3a62f50c0a7438110a2a1b
alan.le <alan.le@95ee034e-633c-0410-b380-e75d38d4a260>
2008-04-13 23:56:39 +0000

    more Witty R&D. added back the testbed project

---
+++ trunk/Witty/WittyTestBed/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WittyTestBed
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TweetScanButton_Click(object sender, RoutedEventArgs e)
+        {
+            tweetscan ts = new tweetscan();
+            ts.Show();
+        }
+
+        private void CustomWindowButton_Click(object sender, RoutedEventArgs e)
+        {
+            CustomWindow cw = new CustomWindow();
+            cw.Show();
+        }
+
+        private void FollowersButton_Click(object sender, RoutedEventArgs e)
+        {
+            Followers f = new Followers();
+            f.Show();
+        }
+    }
+}
