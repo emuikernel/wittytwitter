f646d64681b85fd93e6384a067acf2a74f5f8ed4
scottckoon <scottckoon@95ee034e-633c-0410-b380-e75d38d4a260>
2008-05-07 03:44:19 +0000

    re-organized the ITwitterNet interface to begin refactoring.

--- Witty/TwitterLib/ITwitterNet.cs
+++ Witty/TwitterLib/ITwitterNet.cs
@@ -4,40 +4,45 @@ namespace TwitterLib
     interface ITwitterNet
     {
         Tweet AddTweet(string text);
+
         string ClientName { get; set; }
         string CreateFriendshipUrl { get; set; }
-        User CurrentlyLoggedInUser { get; set; }
-        void DestroyDirectMessage(double id);
+        string SendMessageUrl { get; set; }
+        string UpdateUrl { get; set; }
+        string UserName { get; }
+        string UserShowUrl { get; set; }
+        string UserTimelineUrl { get; set; }
         string DestroyDirectMessageUrl { get; set; }
-        void DestroyTweet(double id);
         string DestroyUrl { get; set; }
         string DirectMessagesUrl { get; set; }
         string FollowersUrl { get; set; }
-        void FollowUser(string userName);
-        string Format { get; set; }
         string FriendsTimelineUrl { get; set; }
         string FriendsUrl { get; set; }
+        string PublicTimelineUrl { get; set; }
+        string RepliesTimelineUrl { get; set; }
+        string Format { get; set; }
+
+        void DestroyDirectMessage(double id);
+        void DestroyTweet(double id);
+        void FollowUser(string userName);
+        void SendMessage(string user, string text);
+
         UserCollection GetFriends(int userId);
         UserCollection GetFriends();
+        User CurrentlyLoggedInUser { get; set; }
+        User GetUser(int userId);
+        User Login();
+
         TweetCollection GetFriendsTimeline();
         TweetCollection GetFriendsTimeline(string since, string userId);
         TweetCollection GetFriendsTimeline(string since);
         TweetCollection GetPublicTimeline(string since);
         TweetCollection GetPublicTimeline();
         TweetCollection GetReplies();
-        User GetUser(int userId);
         TweetCollection GetUserTimeline(string userId);
-        User Login();
-        System.Security.SecureString Password { get; set; }
-        string PublicTimelineUrl { get; set; }
-        string RepliesTimelineUrl { get; set; }
         DirectMessageCollection RetrieveMessages();
-        void SendMessage(string user, string text);
-        string SendMessageUrl { get; set; }
-        string UpdateUrl { get; set; }
-        string UserName { get; }
-        string UserShowUrl { get; set; }
-        string UserTimelineUrl { get; set; }
+
+        System.Security.SecureString Password { get; set; }
         System.Net.IWebProxy WebProxy { get; set; }
     }
 }
